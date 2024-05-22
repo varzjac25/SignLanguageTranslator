9a2778aa29d071d0407fba18bdd450de4882c3f9
Jackson Varzali <117418011+varzjac25@users.noreply.github.com>
2024-05-21 18:17:20 -0700

    Train files for ASL

--- ForeignLanguageTranslation.py
+++ ForeignLanguageTranslation.py
@@ -9,11 +9,37 @@ from pprint import pprint
 # initialize variables and objects
 translator = Translator()
 
+# langDict translates language names into the required shorthand for Google translation
+langDict = {
+
+}
+
 # translateForeign is called from main class
 def translateForeign(input, outLanguage):
 
     # translate to english
     if outLanguage.lower() == "english":
 
+        # translator translates into english by default
         translation = translator.translate(input)
-        
+
+        # return translated text
+        return translation.text
+
+    # translating into languages other than english
+    else:
+
+        # check if language entered is a valid language
+        if langDict.__contains__(outLanguage.lower()):
+
+            # convert language title into shorthand
+
+            # translate language
+            translation = translator.translate(input, dest = "ar")
+
+            # return translation
+            return translation.text
+
+        # return error message
+        else:
+            return "Error: language entered is invalid"
---
+++ Main.py
@@ -0,0 +1,46 @@
+# Jackson Varzali
+# Main runs other python classes based off user input,
+# uses other classes to translate from different language mediums
+
+# imports
+import cv2
+from HandTracker import *
+from ForeignLanguageTranslation import *
+
+# declare variables
+video = False
+readASL = True
+showPoints = False
+foreign = True
+
+# initialize camera
+cam = cv2.VideoCapture(0)
+
+# loop continously runs until broken
+while True:
+
+    # if video is true record and call hand tracker class
+    if video:
+
+        # read camera input
+        success, image = cam.read()
+
+        # calls trackHands from HandTracker class
+        trackHands(readASL, showPoints, image)
+
+    # if foreign is true call ForeignLanguageTranslation to translate
+    elif foreign:
+
+        # call translateForeign from ForeignLanguageTranslation Class
+        # first parameter is input text, second parameter is output language
+        txt = "hola"
+        language = "English"
+        print(translateForeign(txt, language))
+
+    else:
+        break
+
+# After the loop release the cap object
+cam.release()
+# Destroy all the windows
+cv2.destroyAllWindows()
